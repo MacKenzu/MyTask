8935c7e1d500beba2aa2526f0cf00b300e841fd1
MacKenzy <Pa4itsky.jaroslav@gmailcom>
2023-04-13 14:06:10 +0300

    Сравнение футов и км

---
+++ Task_6(Растояние в футах)/KmAndFt.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число в КМ");
+int km = Convert.ToInt32 (Console.Read());
+Console.WriteLine("Введите число в футах");
+int ft = Convert.ToInt32 (Console.Read());
+Double ftt = ft * 0.30;
+if (km < ftt)
+    {
+    Console.WriteLine("Растояние в КМ меньше");
+    }
+else
+{
+    Console.WriteLine($"Растояние в футах меньше = {ftt}");
+}
