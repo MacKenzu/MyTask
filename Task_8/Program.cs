e7e713b9efcbfbd504e43de2dc9305ac4c80c95b
unknown <pa4itsky.jaroslav@gmail.com>
2023-04-18 22:22:33 +0300

    sum 1 do A"

---
+++ Task_8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Демонстрация решения
+// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32 (Console.ReadLine());
+int Getsum()
+{
+    int sum = 0;
+    int count = 0;
+    while (num > count)
+    {
+        count++;
+        sum+= count;
+        
+    }
+    return sum;
+}
+
+Console.WriteLine(Getsum());
