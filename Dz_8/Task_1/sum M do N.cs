b8fe62463de60d366b2cb2ab1d0a2b1a5ee42b98
MacKenzy <Pa4itsky.jaroslav@gmailcom>
2023-05-24 15:29:03 +0300

    sum M do N

---
+++ Dz_8/Task_1/sum M do N.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = ReadInt("Введите число M: ");
+int N = ReadInt("Введите число N: ");
+if (M < N)
+{
+    System.Console.WriteLine(SumNumbers(M, N));
+
+}
+else
+{
+    System.Console.WriteLine(SumNumbers(N, M));
+}
+
+
+
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int M, int N)
+{
+    if (M > N)
+    {
+        return 0;
+    }
+    return M + SumNumbers(M + 1, N);
+}
