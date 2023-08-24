9a9c0dd3d2e51cfce5765bab759c48acd98240dc
MacKenzy <Pa4itsky.jaroslav@gmailcom>
2023-08-24 15:02:48 +0300

    дз первая задача

---
+++ Task_8/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void FillArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        {
+            array [i] = new Random().Next(100);
+        }
+}
+int sum(int [] array )
+{
+ int max = 0;
+ int min = 0;
+ for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        else (array[i] < min)
+        {
+            min = array[i];
+        }
+        int result = max - min;
+        return result;
+    }
+
+}
+int [] myArray = new int [new Random().Next(20)];
+FillArray(myArray);
+Console.WriteLine("["+ String.Join(",", myArray)+ "]");
+Console.WriteLine(sum(myArray));
---
+++ Task_9/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите все значения массива через пробел и нажмите enter");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0 )
+    {
+        count++;
+    }
+}
+Console.WriteLine("["+string.Join(",",array)+"] -> "+count);
