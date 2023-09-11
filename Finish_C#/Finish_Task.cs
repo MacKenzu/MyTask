9c38656c77b978a1ec664a05b74897b8ee83ab2d
MacKenzy <Pa4itsky.jaroslav@gmailcom>
2023-09-11 12:08:36 +0300

    Push

---
+++ Finish_C#/Finish_Task.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите элементы массива через пробел:");
+string input = Console.ReadLine();
+string[] inputArray = input.Split();
+
+if (inputArray.Length > 0)
+{
+    string[] newArray = inputArray.Where(element => element.Length <= 3).ToArray();
+
+    if (newArray.Length > 0)
+    {
+        Console.WriteLine("Новый массив, состоящий из строк, длина которых меньше или равна 3:");
+        string arrayAsString = $"[{string.Join(", ", newArray.Select(element => $"\"{element}\""))}]";
+        Console.WriteLine(arrayAsString);
+    }
+    else
+    {
+        Console.WriteLine("Нет элементов, удовлетворяющих условию.");
+    }
+}
+else
+{
+    Console.WriteLine("Введенный массив пуст.");
+}
